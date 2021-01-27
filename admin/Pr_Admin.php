43457c91a27f908a4e088bb4069630ca6892f670
Marko <upss070288@gmail.com>
2021-01-27 13:13:26 +0100

    Added Settings API, style, js file

--- admin/Pr_Admin.php
+++ admin/Pr_Admin.php
@@ -10,7 +10,16 @@
 
 if ( ! class_exists( 'Pr_Admin' ) ) {
     class Pr_Admin {
+        public function __construct() {
+            if ( is_admin() ) {
+                add_action( 'admin_enqueue_scripts', array( $this, 'pr_enqueue_admin_styles' ) );
+            }
+        }
 
+        public function pr_enqueue_admin_styles() {
+            wp_enqueue_style( 'pr_admin_css', plugins_url( '/assets/css/pr_admin_style.css', __FILE__ ) );
+            wp_enqueue_script( 'pr_admin_js', plugins_url( '/assets/js/pr_admin_script.js', __FILE__ ), array(), '1.0.0', true );
+        }
     }
 
     new Pr_Admin();
--- includes/Pr_Dashboard.php
+++ includes/Pr_Dashboard.php
@@ -10,7 +10,108 @@
 
 if ( ! class_exists( 'Pr_Dashboard' ) ) {
     class Pr_Dashboard {
+        public function __construct() {
+            if ( is_admin() ) {
+                add_action( 'admin_menu', array( $this, 'pr_dashboard' ) );
+                add_action( 'admin_init', array( $this, 'pr_register_settings' ) );
+            }
+        }
 
+        // Adding submenu page
+        public function pr_dashboard() {
+            add_submenu_page( 'tools.php', __( 'Post Redirection', 'post_redirection' ), __( 'Post Redirection', 'post_redirection' ), 'manage_options', 'pr-dashboard', array(
+                $this,
+                'pr_dashboard_page'
+            ) );
+        }
+
+        // Main form on admin part
+        public function pr_dashboard_page() {
+            ?>
+            <div class="wrap">
+                <form action="options.php" method="post">
+
+                    <?php
+                    settings_errors( 'pr_settings_fields' );
+                    settings_fields( 'pr_settings_fields' );
+                    do_settings_sections( 'pr_settings_section_tab' );
+
+                    submit_button(
+                        __( 'Save Changes', 'post_redirection' ),
+                        '',
+                        'pr_save_changes_btn',
+                        true,
+                        array( 'id' => 'pr-save-changes-btn' )
+                    );
+                    ?>
+
+                </form>
+            </div>
+            <?php
+        }
+
+        public function pr_settings_section_callback() {
+            // CHANGE DESCRIPTION LATER
+            _e( 'Set your General settings.', 'post_redirection' );
+        }
+
+        protected function pr_settings_fields( string $type, string $id, string $class, string $name, string $value, $placeholder = '', $description = '', $min = '', $max = '', $required = '' ) {
+            switch ( $type ) {
+                case 'text':
+                    echo '<input type="text" id="' . $id . '" class="' . $class . '" name="pr_settings_fields[' . $name . ']" value="' . $value . '" placeholder="' . __( $placeholder, 'post_redirection' ) . '" ' . $required . '><small class="pr-field-desc">' . __( $description, 'post_redirection' ) . '</small>';
+                    break;
+                case 'number':
+                    echo '<input type="number" id="' . $id . '" class="' . $class . '" name="pr_settings_fields[' . $name . ']" value="' . $value . '" placeholder="' . __( $placeholder, 'post_redirection' ) . '" min="' . $min . '" max="' . $max . '"><small class="pr-field-desc">' . __( $description, 'post_redirection' ) . '</small>';
+                    break;
+                case 'checkbox':
+                    echo '<label class="pr-switch" for="' . $id . '"><input type="checkbox" id="' . $id . '" class="' . $class . '" name="pr_settings_fields[' . $name . ']" value="1" ' . $value . '><span class="pr-slider pr-round"></span></label><small class="pr-field-desc">' . $description . '</small>';
+                    break;
+                case 'url':
+                    echo '<input type="url" id="' . $id . '" class="' . $class . '" name="pr_settings_fields[' . $name . ']" value="' . $value . '"placeholder="' . __( $placeholder, 'post_redirection' ) . '" ' . $required . '><small class="pr-field-desc">' . __( $description, 'post_redirection' ) . '</small>';
+                    break;
+                case 'color':
+                    echo '<input type="color" id="' . $id . '" class="' . $class . '" name="pr_settings_fields[' . $name . ']" value="' . $value . '">';
+                    break;
+                case 'textarea':
+                    echo '<textarea class="' . $class . '" name="pr_settings_fields[' . $name . ']" placeholder="' . __( $placeholder, 'post_redirection' ) . '" id="' . $id . '" rows="7" ' . $required . '>' . $value . '</textarea>';
+                    break;
+            }
+        }
+
+        public function pr_options_check( string $id ): string {
+            $options = get_option( 'pr_settings_fields' );
+
+            return ( ! empty( $options[ $id ] ) ? $options[ $id ] : '' );
+        }
+
+        public function pr_option_check_radio_btn( string $id ): string {
+            $options = get_option( 'pr_settings_fields' );
+
+            return isset( $options[ $id ] ) ? checked( 1, $options[ $id ], false ) : '';
+        }
+
+        // Settings API
+        public function pr_register_settings() {
+
+            register_setting('pr_settings_fields', 'pr_settings_fields', 'pr_sanitize_callback');
+
+            // Adding sections
+            add_settings_section('pr_section_id', __('General', 'post_redirection'), array(
+                $this,
+                'pr_settings_section_callback'
+            ), 'pr_settings_section_tab');
+
+            // General page fields
+            add_settings_field( 'pr_section_id_enabled_disabled', __( 'Enable / Disable', 'post_redirection' ), array(
+                $this,
+                'pr_section_id_enabled_disabled'
+            ), 'pr_settings_section_tab', 'pr_section_id' );
+        }
+
+        // General page Settings API fields
+        public function pr_section_id_enabled_disabled() {
+            $this->pr_settings_fields( 'checkbox', 'pr-enabled-disabled', 'pr-switch-input', 'enabled_disabled', $this->pr_option_check_radio_btn( 'enabled_disabled' ) );
+        }
     }
 
     new Pr_Dashboard();
