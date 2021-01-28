d9069ee686e2c4ec19566a9ea947d33dbe512469
Marko <upss070288@gmail.com>
2021-01-28 12:29:50 +0100

    Data requests send to JS

--- public/Pr_Public.php
+++ public/Pr_Public.php
@@ -10,7 +10,17 @@
 
 if ( ! class_exists( 'Pr_Public' ) ) {
     class Pr_Public {
+        public function __construct() {
+            if ( ! is_admin() ) {
+                echo $this->pr_data_collection( 'end_page', 'pr_hide_end_page' );
+            }
+        }
 
+        public function pr_data_collection( string $id, string $attr_id ): string {
+            $options = get_option( 'pr_settings_fields' );
+
+            return ( ! empty( $options[ $id ] ) ? '<span style="display:none" id="' . $attr_id . '">' . $options[ $id ] . '</span>' : '' );
+        }
     }
 
     new Pr_Public();
---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @link       https://mlab-studio.com
+ * @since      1.0.0
+ *
+ * @package    Post_Redirection
+ */
+
+delete_option( 'pr_settings_fields' );
