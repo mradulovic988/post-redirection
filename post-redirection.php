0baac62391466302b43fc91238bc7039ea21380c
Marko <upss070288@gmail.com>
2021-01-28 15:13:14 +0100

    Settings link for the plugin

--- post-redirection.php
+++ post-redirection.php
@@ -44,6 +44,9 @@ if ( !class_exists( 'Post_Redirection' ) ) {
                 include PR_PLUGIN_PATH . '/admin/Pr_Admin.php';
 
                 $this->pr_load_plugin_textdomain();
+
+                add_filter( 'plugin_action_links', array( $this, 'pr_settings_link' ), 10, 2 );
+                add_filter( 'plugin_row_meta', array( $this, 'pr_set_plugin_meta' ), 10, 2 );
             } else {
                 include PR_PLUGIN_PATH . '/public/Pr_Public.php';
             }
@@ -56,6 +59,39 @@ if ( !class_exists( 'Post_Redirection' ) ) {
                 false,
                 PR_PLUGIN_BASENAME . dirname( __FILE__ ) . '/languages'
             );
+        }
+
+        // Settings link for the plugin
+        public function pr_settings_link( $links, $file ): array {
+            $plugin = plugin_basename( __FILE__ );
+
+            if ( $file == $plugin && current_user_can( 'manage_options' ) ) {
+                array_unshift(
+                    $links,
+                    sprintf( '<a href="%s">' . __( 'Settings', 'post_redirection' ), 'tools.php?page=pr-dashboard' ) . '</a>'
+                );
+            }
+
+            return $links;
+        }
+
+        // Settings link for the plugin
+        public function pr_set_plugin_meta( $links, $file ): array {
+            $plugin = plugin_basename( __FILE__ );
+
+            if ( $file == $plugin && current_user_can( 'manage_options' ) ) {
+                array_push(
+                    $links,
+                    sprintf( '<a target="_blank" href="%s">' . __( 'Docs & FAQs', 'post_redirection' ) . '</a>', 'https://wordpress.org/support/plugin/post-redirection' )
+                );
+
+                array_push(
+                    $links,
+                    sprintf( '<a target="_blank" href="%s">' . __( 'GitHub', 'post_redirection' ) . '</a>', 'https://github.com/mradulovic988/post-redirection' )
+                );
+            }
+
+            return $links;
         }
     }
 
