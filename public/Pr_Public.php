801ead5e4eaf4c2bf3ed592896bd4e1a8c324536
Marko <upss070288@gmail.com>
2021-01-29 13:12:25 +0100

    Fixed bug with post updating - Need further testing on this

--- public/Pr_Public.php
+++ public/Pr_Public.php
@@ -14,17 +14,29 @@ if ( ! class_exists( 'Pr_Public' ) ) {
             if ( ! is_admin() ) {
                 $option_data = get_option( 'pr_settings_fields' ); // Get option data
 
-                echo $this->pr_data_collection( 'end_page', 'pr_hide_end_page' ); // Sending data to front-end
+                // *** BUG -> Can't add/edit/update posts ***
+//                echo $this->pr_data_collection( 'end_page', 'pr_hide_end_page' ); // Sending data to front-end
 
                 // If enabled/disabled field is turned on enqueue the script
                 if ( $option_data[ 'enabled_disabled' ] == 1 ) {
                     add_action( 'wp_enqueue_scripts', array( $this, 'pr_enqueue_public_styles' ) );
                 }
+
+                add_action( 'wp_head', function() {
+                    $options = get_option( 'pr_settings_fields' );
+
+                    echo ( ! empty( $options[ 'end_page' ] ) ? '<span style="display:none" id="pr_hide_end_page">' . $options[ 'end_page'  ] . '</span>' : '' );
+                });
             }
         }
 
+        public function send_data() {
+            $option_data = get_option( 'pr_settings_fields' ); // Get option data
+            echo $this->pr_data_collection( 'end_page', 'pr_hide_end_page' );
+        }
+
         // Collect data from option data
-        public function pr_data_collection( string $id, string $attr_id = '' ): string {
+        public function pr_data_collection( string $id, string $attr_id ): string {
             $options = get_option( 'pr_settings_fields' );
 
             return ( ! empty( $options[ $id ] ) ? '<span style="display:none" id="' . $attr_id . '">' . $options[ $id ] . '</span>' : '' );
